05fdf9784c79e056bc71c84e181a1ba9afdf212e
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-25 20:54:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,9 +4,10 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect do |calls| 
-    puts "#{calls}.upcase" 
-end
+  array.collect do |calls|
+    puts "#{calls}.upcase"
+  end
+end 
 
 def long_planeteer_calls# code an argument here
   # Your code here
