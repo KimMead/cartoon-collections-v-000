ea94923e9aefdb0927dc63faa1e02bf87d40a950
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-25 21:31:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.include? do |cheese|
+    return "#{cheese}"
+    
 end
