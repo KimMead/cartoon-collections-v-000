484df568d62d3dd40a2deb07c947ff96f3d563a9
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-25 21:01:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 def summon_captain_planet(array)
   planeteer_calls = []
   array.collect do |calls|
-    planeteer_calls << "#{calls.upcase}!"
+    planeteer_calls << "#{calls.upcase.first}!"
   end
 end
 
